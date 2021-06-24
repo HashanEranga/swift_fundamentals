3b0c21dceceb9dab2c54d392868658de74782f3a
Hashan Eranga <ccmrhe@gmail.com>
2021-06-25 05:02:46 +0530

    build(Day2) : Day 2 elements are added

---
+++ Lesson2/1_lists.swift
@@ -0,0 +1,11 @@
+let shoppingList : [String] = ["egg", "vegitables"]
+print(shoppingList)
+
+var variableList : [String] = ["egg", "vegetables"]
+print(variableList)
+
+variableList.append("pots")
+print(variableList)
+
+// shoppingList.append("pots")
+// print(shoppingList)
